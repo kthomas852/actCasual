0195fa18be2670ec997fd3c8125aef1daf7f2670
Kyle Thomas <kthomas852@yahoo.com>
2018-11-03 19:44:34 -0700

    update

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,44 +1,44 @@
 var members = [
     {
         name: "Ashley Garcia",
-        photo: "",
-        sex: "1",
+        photo: "https://thenypost.files.wordpress.com/2018/05/kaminmohammadi.jpg?quality=90&strip=all&w=618&h=410&crop=1",
+        sex: "men",
         questions: ["4", "3", "5", "2", "3", "1", "1", "4", "5"]
     },
     {
         name: "Me Myself and I",
-        photo: "",
-        sex: "1",
+        photo: "https://cdn4.vectorstock.com/i/1000x1000/02/18/man-with-question-mark-flat-icon-pictogram-vector-4920218.jpg",
+        sex: "both",
         questions: ["1", "1", "1", "1", "1", "1", "1", "1", "1"]
     },
     {
         name: "Dexter Progeens",
-        photo: "",
-        sex: "3",
+        photo: "https://s3-media4.fl.yelpcdn.com/buphoto/l4zKHiX9tHCI5S48t3U7-Q/o.jpg",
+        sex: "women",
         questions: ["2", "3", "1", "1", "1", "1", "3", "4", "2"]
     },
     {
         name: "Courtney Humphries",
-        photo: "",
-        sex: "1",
+        photo: "https://ichef.bbci.co.uk/news/660/cpsprodpb/FD27/production/_101970846_aubreyblanche.jpg",
+        sex: "men",
         questions: ["5", "4", "5", "2", "3", "4", "4", "1", "1"]
     },
     {
         name: "Braxton Hynes",
-        photo: "",
-        sex: "1",
+        photo: "http://static1.uk.businessinsider.com/image/5748a50cdd0895c4148b487d/scientifically-proven-features-men-find-attractive-in-women.jpg",
+        sex: "men",
         questions: ["4", "2", "2", "4", "3", "5", "5", "2", "1"]
     },
     {
         name: "Jesus Brosbeem",
-        photo: "",
-        sex: "2",
+        photo: "https://hips.hearstapps.com/hbz.h-cdn.co/assets/16/15/hbz-hot-guys-on-instagram-06.jpg",
+        sex: "women",
         questions: ["5", "4", "5", "1", "3", "3", "3", "2", "1"]
     },
     {
         name: "Tori Forest",
-        photo: "",
-        sex: "3",
+        photo: "https://www.dillards.com/images/espots/102318_women_dillards_department_page_feature_womens_tops.jpg",
+        sex: "both",
         questions: ["5", "5", "5", "2", "1", "5", "5", "4", "5"]
     }
 ];
